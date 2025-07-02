30215d3dcbc959a50a01246e0267b3201adfd370
VishalGhuge111 <vishalghugesit@gmail.com>
2025-07-02 15:48:58 +0530

    bug fixed

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,15 @@
 // Firebase configuration
+// Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyC7EvohC3Y9FScY0glVt-hPvkPWrbdM0rU",
   authDomain: "sppu-engineers-111.firebaseapp.com",
   databaseURL: "https://sppu-engineers-111-default-rtdb.firebaseio.com",
   projectId: "sppu-engineers-111",
-  storageBucket: "sppu-engineers-111.firebasestorage.app",
+  storageBucket: "sppu-engineers-111.appspot.com", // ✅ Fixed this line
   messagingSenderId: "622650122117",
   appId: "1:622650122117:web:0218bbf87b43cf1168aeec",
-}
+};
+
 
 // Initialize Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"

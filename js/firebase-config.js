64cd9c233d781bb5de98909267a6d1eed094c8ad
VishalGhuge111 <vishalghugesit@gmail.com>
2025-07-02 16:02:13 +0530

    bug fixed

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,17 @@
-// js/firebase-config.js
+// Firebase configuration
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_REAL_API_KEY",
+  apiKey: "AIzaSyC7EvohC3Y9FScY0glVt-hPvkPWrbdM0rU",
   authDomain: "sppu-engineers-111.firebaseapp.com",
   databaseURL: "https://sppu-engineers-111-default-rtdb.firebaseio.com",
   projectId: "sppu-engineers-111",
-  storageBucket: "sppu-engineers-111.appspot.com", // ✅ must be .appspot.com
+  storageBucket: "sppu-engineers-111.firebasestorage.app",
   messagingSenderId: "622650122117",
-  appId: "1:622650122117:web:0218bbf87b43cf1168aeec"
-};
+  appId: "1:622650122117:web:0218bbf87b43cf1168aeec",
+}
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+// Initialize Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
 
-const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+const app = initializeApp(firebaseConfig)
+export const database = getDatabase(app)
